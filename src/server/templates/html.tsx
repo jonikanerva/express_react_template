8f077a83bf26fa6332fd88c6e9fe1f4344eaf879
Joni Kanerva <jhonny@jhonny.org>
2021-03-20 17:22:51 +0200

    dirty fix something adds "auto" in front of the bundle filenames, couldn't figure out what it was quickly, so this is here for now. sry.

--- src/server/templates/html.tsx
+++ src/server/templates/html.tsx
@@ -3,7 +3,7 @@ import { renderToString } from 'react-dom/server'
 import App from '../../components/App'
 import manifestJson from '../../../manifest.json'
 
-const manifest: { [key: string]: string } = manifestJson
+const manifest: Record<string, string> = manifestJson
 
 const html = (): string => `<!DOCTYPE html>
 <html lang="en">
@@ -11,11 +11,11 @@ const html = (): string => `<!DOCTYPE html>
   <title>Hello World</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-  <link rel="stylesheet" href="/${manifest['main.css']}" />
+  <link rel="stylesheet" href="/${manifest['main.css'].replace(/^auto/, '')}" />
 </head>
 <body>
   <div id="root">${renderToString(<App />)}</div>
-  <script src="/${manifest['main.js']}"></script>
+  <script src="/${manifest['main.js'].replace(/^auto/, '')}"></script>
 </body>
 </html>`
 
